a36216025f27bd2843bc7ee8f9e19b8f5e2291d5
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-06-02 10:56:11 +0200

    create blog service implemented
    with client side, now it is possible to create new blog

---
+++ src/main/java/blog/client/BlogClient.java
@@ -0,0 +1,54 @@
+package blog.client;
+
+import com.proto.blog.Blog;
+import com.proto.blog.BlogId;
+import com.proto.blog.BlogServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+public class BlogClient {
+
+    private static BlogId createBlog(BlogServiceGrpc.BlogServiceBlockingStub stub) {
+        try {
+            BlogId createResponse = stub.createBlog(
+                    Blog.newBuilder()
+                            .setAuthor("Tomas")
+                            .setTitle("New Blog")
+                            .setContent("This is the new blog... stand up and admit it.")
+                            .build()
+            );
+
+            System.out.println("Blog create: " + createResponse.getId());
+            return createResponse;
+
+        } catch (StatusRuntimeException e) {
+            System.out.println("Could not create blog");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void run(ManagedChannel channel) {
+
+        BlogServiceGrpc.BlogServiceBlockingStub stub = BlogServiceGrpc.newBlockingStub(channel);
+
+        BlogId blogId = createBlog(stub);
+
+        if (blogId == null) {
+            return;
+        }
+    }
+
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+        run(channel);
+
+        System.out.println("Shutting down");
+        channel.shutdown();
+    }
+}
